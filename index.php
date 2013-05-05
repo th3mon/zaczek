75f0ae01ac850326aed3a524e7d0731bde8c1304
th3mon <p.szelenberger@gmail.com>
2013-05-05 16:16:57 +0200

    Delete test code

--- index.php
+++ index.php
@@ -5,12 +5,6 @@
 				<div id="inner-content" class="wrap clearfix">
 
 						<div id="main" class="eightcol first clearfix" role="main">
-							<?php
-								$pagename = $wp_query->page;
-								$test = "test";
-							?>
-							<h1 class="h1"><?php echo "test"; ?></h1>
-
 							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
